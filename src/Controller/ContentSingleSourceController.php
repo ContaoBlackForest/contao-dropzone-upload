7320bfc4396740981eb42ebce69a47297a3d2ab5
Sven Baumann <baumann.sv@gmail.com>
2016-08-26 01:43:45 +0200

    add content single source controller

---
+++ src/Controller/ContentSingleSourceController.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Copyright © ContaoBlackForest
+ *
+ * @package   contao-dropzone-upload
+ * @author    Sven Baumann <baumann.sv@gmail.com>
+ * @author    Dominik Tomasi <dominik.tomasi@gmail.com>
+ * @license   GNU/LGPL
+ * @copyright Copyright 2014-2016 ContaoBlackForest
+ */
+
+namespace ContaoBlackForest\DropZone\Controller;
+
+use Contao\FileTree;
+
+class ContentSingleSourceController extends AbstractController
+{
+    protected $parseWidget = 'content.singleSRC';
+
+    protected $folder = 'files/tiny_templates';
+
+    protected $uploadMultiple = 'false';
+
+    public function injectDropZone($buffer, FileTree $widget)
+    {
+        return parent::injectDropZone($buffer, $widget);
+    }
+}
