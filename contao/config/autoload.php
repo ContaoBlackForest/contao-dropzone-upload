dafe54c51f705030e34f1c6ebe97782fa17da5d8
Sven Baumann <baumann.sv@gmail.com>
2016-08-26 01:23:42 +0200

    add the backend template

---
+++ contao/config/autoload.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Copyright © ContaoBlackForest
+ *
+ * @package   contao-dropzone-upload
+ * @author    Sven Baumann <baumann.sv@gmail.com>
+ * @author    Dominik Tomasi <dominik.tomasi@gmail.com>
+ * @license   GNU/LGPL
+ * @copyright Copyright 2014-2016 ContaoBlackForest
+ */
+
+/**
+ * load templates
+ */
+\Contao\TemplateLoader::addFiles(
+    array(
+        'be_image_dropzone' => 'system/modules/dropzone-upload/templates/backend',
+    )
+);
+
+
