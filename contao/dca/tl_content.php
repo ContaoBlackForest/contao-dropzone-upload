1ce233ee7fd47bb3c2dac935a37908df9f42868a
Sven Baumann <baumann.sv@gmail.com>
2016-08-26 01:41:58 +0200

    configure tl_content

---
+++ contao/dca/tl_content.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Copyright © ContaoBlackForest
+ *
+ * @package   contao-dropzone-upload
+ * @author    Sven Baumann <baumann.sv@gmail.com>
+ * @author    Dominik Tomasi <dominik.tomasi@gmail.com>
+ * @license   GNU/LGPL
+ * @copyright Copyright 2014-2016 ContaoBlackForest
+ */
+
+/**
+ * inject the drop zone for content elements
+ */
+$GLOBALS['TL_DCA']['tl_content']['fields']['singleSRC']['load_callback'][] = array(
+    'ContaoBlackForest\DropZone\Controller\ContentSingleSourceController',
+    'initializeParseWidget'
+);
+
+$GLOBALS['TL_DCA']['tl_content']['config']['onload_callback'][] = array(
+    'ContaoBlackForest\DropZone\Data\Store',
+    'parse'
+);
