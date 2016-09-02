ac839d6e25b412ba145cfe5bce9d1b2323eed682
Sven Baumann <baumann.sv@gmail.com>
2016-09-02 12:35:46 +0200

    register the subscriber

---
+++ Resources/config/event_subscribers.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Copyright © ContaoBlackForest
+ *
+ * @package   contao-dropzone-upload
+ * @author    Sven Baumann <baumann.sv@gmail.com>
+ * @author    Dominik Tomasi <dominik.tomasi@gmail.com>
+ * @license   GNU/LGPL
+ * @copyright Copyright 2014-2016 ContaoBlackForest
+ */
+
+use ContaoBlackForest\DropZoneBundle\DataContainer\Table\Content;
+
+return array(
+    new Content()
+);
